e6573c9040a99a14cb8ebbc983df6cf6f893fda7
archit-kc <archit@karmacircles.com>
2021-06-18 16:56:03 +0530

    Added GameOverScreen to App.js

--- GuessNumber/App.js
+++ GuessNumber/App.js
@@ -16,24 +16,38 @@ import {
 import Header from './components/Header';
 import StartScreen from './components/StartScreen';
 import GameScreen from './components/GameScreen';
+import Gameover from './components/Gameover';
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-      selected : undefined
+      selected : undefined,
+      guessround : 0
     }
   }
 
+  newgamehandler(){
+    this.setState({guessround : 0});
+    this.setState({selected : null});
+  }
+
   startscreenhandler(number){
+    console.log(number);
     this.setState({selected : number}); 
   }
   
 
   renderScreen(){
-    if(this.state.selected){
-      const component = <GameScreen userchoice={this.state.selected}></GameScreen>
+    if(this.state.selected && this.state.guessround <= 0){
+      const component = <GameScreen userchoice={this.state.selected} onOver={(rounds) => {
+        this.setState({guessround : rounds});
+      }}></GameScreen>
+      return component;
+    }
+    else if(this.state.guessround > 0){
+      const component = <Gameover rounds={this.state.guessround} usernumber={this.state.selected} onRestart={this.newgamehandler.bind(this)}></Gameover>;
       return component;
     }
     else{
